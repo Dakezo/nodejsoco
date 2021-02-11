f892aafa3237f829959666352fb9d88f95d405c5
Dakezo <s.g.oconnell@gmail.com>
2021-02-11 15:02:26 -0500

    removing app/app1 references for now

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const http = require('http');
-const app1 = require('./app1');
-const app = require('./app');
 
 const hostname = '172.23.18.80';
 const port = 443;
@@ -14,20 +12,4 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
-});
-
-const app.get('/', (app: any, res) => {
-    res.app
-});
-
-app.listen(443, () => {
-    console.log('App is listening at http://'{ localhost }':$'{ port })
-});
-
-const app1.get('/', (app1:any, res) => {
-    res.app
-});
-
-app1.listen(443, () => {
-    console.log('App1 is listening at http://'{ localhost }':$'{ port })
 });
