baea84da636d3520e9a98a54edb9181ecfa38fd2
Dakezo <s.g.oconnell@gmail.com>
2021-02-10 19:43:42 -0500

    making this up as i go

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const http = require('http');
+const app = require('app1.js');
+const app = require('app.js');
 
 const hostname = '172.23.18.80';
 const port = 443;
@@ -6,7 +8,7 @@ const port = 443;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Checking...Lunchtime.');
+    res.end('Hello World');
 });
 
 server.listen(port, hostname, () => {
