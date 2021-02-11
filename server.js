b5c333d28133a358b9baace012067f788208e3cc
Dakezo <s.g.oconnell@gmail.com>
2021-02-11 11:15:44 -0500

    dfxcv

--- server.js
+++ server.js
@@ -16,18 +16,18 @@ server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
 
-const app.get('/', (req:app, res) => {
+const app.get('/', (app: any, res) => {
     res.app
 });
 
 app.listen(443, () => {
-    console.log('Example Port is listening at http://'{ localhost }':$'{ port })
+    console.log('App is listening at http://'{ localhost }':$'{ port })
 });
 
-const app1.get('/', (req: app, res) => {
+const app1.get('/', (app1:any, res) => {
     res.app
 });
 
 app1.listen(443, () => {
-    console.log('Example Port is listening at http://'{ localhost }':$'{ port })
+    console.log('App1 is listening at http://'{ localhost }':$'{ port })
 });
