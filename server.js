173033b4ff89c1e1ee1f69f633efea1ca00e4e30
Dakezo <s.g.oconnell@gmail.com>
2021-02-11 11:11:30 -0500

    trying to resolve exports problems

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http');
-const http = require('./app1');
-const http = require('./app');
+const app1 = require('./app1');
+const app = require('./app');
 
 const hostname = '172.23.18.80';
 const port = 443;
@@ -11,6 +11,23 @@ const server = http.createServer((req, res) => {
     res.end('Checking....Lunch Time.');
 });
 
+
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+const app.get('/', (req:app, res) => {
+    res.app
+});
+
+app.listen(443, () => {
+    console.log('Example Port is listening at http://'{ localhost }':$'{ port })
+});
+
+const app1.get('/', (req: app, res) => {
+    res.app
+});
+
+app1.listen(443, () => {
+    console.log('Example Port is listening at http://'{ localhost }':$'{ port })
 });
